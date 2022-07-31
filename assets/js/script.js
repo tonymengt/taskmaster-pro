40535ed3709ac3881e84e92f4e17ea512957a2b7
Tmeng <tonymengt@gmail.com>
2022-07-31 15:20:02 -0400

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -147,16 +147,18 @@ $(".card .list-group").sortable({
   tolerance:"pointer",
   helper: "clone",
   activate: function (event) {
-    // console.log("active", this);
+    $(this).addClass("dropover")
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function (event) {
-    // console.log("deactivate", this);
+    $(this).removeClass("dropover")
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function (event) {
-    // console.log("over", event.target);
+    $(event.target).addClass("dropover-active")
   },
   out: function (event) {
-    // console.log("out", event.target);
+    $(event.target).removeClass("dropover-active")
   },
   update: function (event) {
     // array to store the task data in
@@ -199,13 +201,14 @@ $("#trash").droppable({
   tolerance: "touch",
   drop: function (event, ui) {
     ui.draggable.remove()
+    $(".bottom-trash").removeClass("bottom-trash-active");
     // console.log("drop");
   },
   over: function (event, ui){
-    // console.log("over");
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function (event, ui){
-    // console.log("out");
+    $(".bottom-trash").removeClass("bottom-trash-active");
   }
 });
 
@@ -228,6 +231,12 @@ var auditTask = function(taskEl) {
     }
 }
 
+setInterval(function() {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, (1000*60)*30);
+
 // adding datpicker
 $("#modalDueDate").datepicker({
   minDate:1
@@ -246,7 +255,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
